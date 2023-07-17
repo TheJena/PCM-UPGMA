db703cbdcd1d029d1389f4b1f2de8938e0afc350
Lorenzo Carletti <lorenzo.carletti@unimore.it>
2023-07-17 07:16:53 +0200

    Separate plot creation and implement for all
    Signed-off-by: Lorenzo Carletti <lorenzo.carletti@unimore.it>

---
+++ create_plots.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# coding: utf-8
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# Copyright (C) 2023 Federico Motta    <federico.motta@unimore.it>
+#                    Lorenzo  Carletti <lorenzo.carletti@unimore.it>
+#                    Matteo   Vanzini  <matteo.vanzini@unimore.it>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+   Build a dendrogram by running UPGMA over some language parameters
+
+   UPGMA (Unweighted Pair Group Method with Arithmetic mean) is a
+   simple agglomerative (bottom-up) hierarchical clustering method
+
+   Usage:
+            coming soon...
+"""
+import numpy as np
+import pandas as pd
+
+from matplotlib import pyplot as plt
+from scipy.cluster.hierarchy import dendrogram
+from sklearn.cluster import AgglomerativeClustering
+
+
+def plot_dendrogram(model, **kwargs):
+    """Create linkage matrix and then plot the dendrogram"""
+
+    # create the counts of samples under each node
+    counts = np.zeros(model.children_.shape[0])
+    n_samples = len(model.labels_)
+    for i, merge in enumerate(model.children_):
+        current_count = 0
+        for child_idx in merge:
+            if child_idx < n_samples:
+                current_count += 1  # leaf node
+            else:
+                current_count += counts[child_idx - n_samples]
+        counts[i] = current_count
+
+    linkage_matrix = np.column_stack(
+        [model.children_, model.distances_, counts]
+    ).astype(float)
+
+    # Plot the corresponding dendrogram
+    dendrogram(linkage_matrix, **kwargs)
+
+base_folder = "datasets/"
+base_file = ["preprocessed_inputs_01.xlsx", "preprocessed_inputs_02.xlsx", "preprocessed_inputs_03.xlsx", "preprocessed_inputs_04.xlsx"]
+sheet_name = ["Foglio1", "Foglio1", "Foglio1", "no_all_zero_rules_imputed_PGE_W"]
+need_to_transpose = [False, False, False, True]
+unallowed_char = ["?", "?", "?", "0"]
+output_names = ["TMP", "TMP", "TMP", "32 dialetti, from 'TableA 2023', imputed (PGE=-, WAP=++-, GFL=-, PGL=+)"]
+index = 3
+
+dfs = pd.read_excel(base_folder + base_file[index], sheet_name=None)
+df = dfs[sheet_name[index]]
+
+df_t = df
+if need_to_transpose[index]:
+    df_t = df.transpose()
+
+# Row/Column index removal
+if need_to_transpose[index]:
+    df_t.columns = df_t.iloc[0, :].to_list()
+    df_t = df_t.iloc[list(range(1, 33)), :]
+else:
+    df_t.index = df_t.iloc[: , 0].to_list()
+    df_t = df_t.iloc[: , list(range(1, len(df_t.keys())))]
+
+selector = [param for param, flag in df_t.astype(str).eq(unallowed_char[index]).sum(axis=0).eq(0).to_dict().items() if flag]
+df_bool_imputed = df_t.loc[:, selector].replace({"+": True, "-": False, "1": True, "0": False, 1: True, 0: False}).astype("boolean")
+
+df_clust = df_bool_imputed.astype(int)
+model = AgglomerativeClustering(linkage="average", metric="euclidean", distance_threshold=0, n_clusters=None)
+model.fit(df_clust)
+plt.clf()
+plt.cla()
+plt.title("Hierarchical Clustering Dendrogram\n(UPGMA method)")
+plot_dendrogram(model, labels=list(df_clust.transpose().columns))
+plt.xlabel(output_names[index])
+plt.savefig("./plot2.svg", dpi=1200)
